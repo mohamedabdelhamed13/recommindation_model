a1b489d4bbf2d8f090007d20116f399a280582e8
Muhammed Abdelhamid <67540846+mohamedabdelhamed13@users.noreply.github.com>
2023-06-23 12:01:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,17 +14,15 @@ def get_data():
         query = pd.get_dummies(pd.DataFrame(json_))
         query = query.reindex(columns=['id','prv'], fill_value=0)
     
-        data = ['132793040','321732944','439886341','511189877','528881469','059400232X','594012015','594033926','594296420'
-                 ,'594450209','594451647','594481813','594481902','089933623X','777700018','094339676X','970407998','970408005'
-                 ,'972683275','986987662','1034385789','1182702627']
+        data = ['MAxpxbAc7gPmkGPob7cVgIlMXaN2','Vl4gTpN7bjPcQbWMGfWEkzZDlDw2','gKCTianp3zVQhhk96Yta9ACtcow2','mV45e05UzYVr3sfnQ8G4Vzh94KG2','vtjNiOr9ejQn7NBGV5wswmQdWEm2','4Hy9C5a9n5WTAG9CcB4I9QWeIrh2'
+                 ,'WkIoaJMjODc4OaOtftW8qbCqjCl2','izbMstyksnNKxF8fBYmO1dKbLXO2','3VgonS0NDQWaMYnewfbUEervMin1','XYrBgvuwpuXdcoh9E8mJj0r0qAP2']
         predict=np.random.choice(data,size=5)
         return jsonify( {"prediction": str(predict)})
     except:
-        data = ['132793040','321732944','439886341','511189877','528881469','059400232X','594012015','594033926','594296420'
-                 ,'594450209','594451647','594481813','594481902','089933623X','777700018','094339676X','970407998','970408005'
-                 ,'972683275','986987662','1034385789','1182702627']
+        data = ['MAxpxbAc7gPmkGPob7cVgIlMXaN2','Vl4gTpN7bjPcQbWMGfWEkzZDlDw2','gKCTianp3zVQhhk96Yta9ACtcow2','mV45e05UzYVr3sfnQ8G4Vzh94KG2','vtjNiOr9ejQn7NBGV5wswmQdWEm2','4Hy9C5a9n5WTAG9CcB4I9QWeIrh2'
+                 ,'WkIoaJMjODc4OaOtftW8qbCqjCl2','izbMstyksnNKxF8fBYmO1dKbLXO2','3VgonS0NDQWaMYnewfbUEervMin1','XYrBgvuwpuXdcoh9E8mJj0r0qAP2']
         predict=np.random.choice(data,size=5)
-        return jsonify(predict)
+        return jsonify( {"prediction": [str(predict[0]),str(predict[1]),str(predict[2]),str(predict[3]),str(predict[4])]})
 
 if __name__ == '__main__':
     app.run(debug=True)
