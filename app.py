b73ca50bb077de47e1205f3703498ae51b2cc243
Muhammed Abdelhamid <67540846+mohamedabdelhamed13@users.noreply.github.com>
2023-06-23 10:22:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def get_data():
                  ,'594450209','594451647','594481813','594481902','089933623X','777700018','094339676X','970407998','970408005'
                  ,'972683275','986987662','1034385789','1182702627']
         predict=np.random.choice(data,size=5)
-        return jsonify( {"prediction": predict})
+        return jsonify(predict)
 
 if __name__ == '__main__':
     app.run(debug=True)
